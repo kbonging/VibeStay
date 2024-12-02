b40761953145a87f8cdc30dd808a17a5c003a22f
bongjung kim <apple75391@gmail.com>
2024-12-02 13:49:32 +0900

    first commit!!!

---
+++ VibeStay/src/test/java/com/project/VibeStay/VibeStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.VibeStay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
