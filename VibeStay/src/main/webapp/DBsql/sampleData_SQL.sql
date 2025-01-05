af6cf6f53645d0781522c5cb50cd8e98a55e60d1
bongjung kim <apple75391@gmail.com>
2025-01-05 19:05:47 +0900

    laptop > desktop - bong(25.01.05)

--- VibeStay/src/main/webapp/DBsql/sampleData_SQL.sql
+++ VibeStay/src/main/webapp/DBsql/sampleData_SQL.sql
@@ -1,6 +1,10 @@
 #############################  tb_member 테이블 샘플 데이터 #############################
 INSERT INTO tb_member (MEMBER_ID, MEMBER_PWD, MEMBER_NAME, MEMBER_EMAIL,  REG_DATE, MOD_DATE) 
-VALUES ('admin', '$2b$12$5D5ZkhMD/gRway0KpmIzzOtfLFWnoiwEIZxGryc4eKhUzxG.Bktyu', '관리자', 'apple75391@gmail.com', NOW(), NULL);
+VALUES ('superadmin', '$2b$12$0pslUhe1HCugHukoKkvJD.JQY/8Aavh6t6ZdekC6ZroZuCfLz.40C', '메인관리자', 'apple75391@gmail.com', NOW(), NULL);
 #####################################################################################
 select * from tb_member;
+
+
+
+
 
